28085675cbdcc5a83a04b9551b7865b7cc94a7f1
OlhaHupaliuk bc2024-3 <olha.hupaliuk@lnu.edu.ua>
2024-09-28 15:04:55 +0300

    Перша частина

---
+++ app.js
@@ -0,0 +1,56 @@
+const {program} = require('commander');
+const fs = require('fs');
+
+
+program
+   .requiredOption('-i, --input <path>', 'шлях до файлу, який даємо для читання')
+   .option('-o, --output <path>', 'шлях до файлу, y якому записуємо результат')
+   .option('-d, --display','результат має бути виведено у консоль');
+
+   
+program.parse(process.argv)
+   
+const options = program.opts();
+
+function exit(message) {
+   console.error(message);
+   process.exit(1);
+}
+
+if (!options.input) {
+    exit("Please, specify input file")
+}
+
+if (!fs.existsSync(options.input)){
+    exit("Cannot find input file")
+}
+
+fs.readFile(options.input, 'utf8', (err, data) => {
+    if (err){
+        exit('Error')
+    }
+    try {
+        const jsonData = JSON.parse(data);
+        const result = JSON.stringify(jsonData, null, 2);
+
+        if(options.display){
+            console.log(result);
+        }
+
+        if(options.output){
+            fs.writeFile(options.output, result, (err) => {
+                if (err) {
+                    exit("Err")
+                }
+                else{
+                    console.log('Results are succesfully saved into file ' + options.output);
+                }
+            })
+        }
+    }
+    catch (err){
+        exit("Error during parsing json")
+    }
+});
+
+
